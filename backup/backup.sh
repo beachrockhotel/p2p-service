f6d59718100674c7cd56b41d3d6a67a4bf2019b8
beachrockhotel <d.komarov2019@gmail.com>
2025-06-08 18:57:45 +0300

    backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-rsync -av /etc/hosts ~/projects/p2p-service/backup/hosts.bak >> ~/projects/p2p-service/backup/backup.log
-echo "[$(date)] Backup completed." >> ~/projects/p2p-service/backup/backup.log
+
+# Переменные
+SRC_DIR="/home/dmitriy/projects/p2p-service"
+BACKUP_DIR="/home/dmitriy/projects/p2p-service/backup"
+TIMESTAMP=$(date "+%Y-%m-%d_%H-%M-%S")
+ARCHIVE_NAME="backup_$TIMESTAMP.tar.gz"
+
+# Лог
+echo "[$(date '+%F %T')] Начало резервного копирования" >> "$BACKUP_DIR/backup.log"
+
+# Создание архива
+tar -czf "$BACKUP_DIR/$ARCHIVE_NAME" -C "$SRC_DIR" .
+
+# Проверка результата
+if [ $? -eq 0 ]; then
+  echo "[$(date '+%F %T')] Архив создан: $ARCHIVE_NAME" >> "$BACKUP_DIR/backup.log"
+else
+  echo "[$(date '+%F %T')] Ошибка при создании архива" >> "$BACKUP_DIR/backup.log"
+fi
+
+echo "[$(date '+%F %T')] Завершено" >> "$BACKUP_DIR/backup.log"
+
+find "$BACKUP_DIR" -name 'backup_*.tar.gz' -mtime +3 -delete
